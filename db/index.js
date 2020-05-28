49d4616401d5ab4547e6db2ed5f022adfb9b537e
loisaleghe <loisaleghe@cmail.carleton.ca>
2020-05-28 10:08:35 -0400

    updated the add button, connected the sql to the handlebars and api

--- db/index.js
+++ db/index.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2')
 const defaultConfig = {
   host: 'localhost',
   user: 'root',
-  password: '' //pick a password
+  password: 'Slois123.' //pick a password
 }
 
 class DB {
